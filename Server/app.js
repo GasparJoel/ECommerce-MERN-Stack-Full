8c84b741b844f7e0b05f84ac0f7c033ffaae66c2
Joel Gt <0332201030@unjfsc.edu.pe>
2024-10-08 21:45:31 -0500

    Implemente los midlewares que se van a usar como el cors , morgan. cors, y en cors agregue los encabezados , metodos permitidos , la URL del client

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,23 @@
 import  express  from "express";
-
+import  cookieParser  from "cookie-parser";
+import  cors  from "cors";
+import morgan from "morgan";
 const app = express()
 
 
-
+app.use(cors({
+    origin:'http://localhost:5173/',
+    methods: ['GET','POST','DELETE','PUT'],
+    allowedHeaders:[
+        "Content-type",
+        "Authorization",
+        'Cache-Control',
+        'Expires',
+        'Pragma'
+    ],
+    credentials :true
+}))
+app.use(cookieParser())
+app.use(express.json())
+app.use(morgan('dev'))
 export default app
