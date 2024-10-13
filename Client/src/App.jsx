5efad96bb7f8a6833844c89ddf5f456e7e42c94e
Joel Gt <0332201030@unjfsc.edu.pe>
2024-10-12 23:57:47 -0500

    Integre el layout donde abarcara todas las vistasd del lado amdinistrador

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom"
 import { AuthLayout } from "./components/auth/layout"
 import { AuthLogin } from "./pages/auth/login"
 import { AuthRegiter } from "./pages/auth/register"
+import { AdminDashboard } from "./pages/admin-view/dashboard"
+import { AdminProducts } from "./pages/admin-view/products"
+import { AdminOrders } from "./pages/admin-view/orders"
+import { AdminFeatures } from "./pages/admin-view/features"
+import { AdminLayout } from "./pages/admin-view/layout"
 
 
 function App() {
@@ -19,7 +24,10 @@ function App() {
         </Route>
 
         <Route path="/admin" element={<AdminLayout/>} >
-
+          <Route path="dashboard" element={<AdminDashboard/>} />
+          <Route path="products" element={<AdminProducts/>} />
+          <Route path="orders" element={<AdminOrders/>} />
+          <Route path="features" element={<AdminFeatures/>} />
         </Route>
 
        
---
+++ Client/src/pages/admin-view/layout.jsx
@@ -0,0 +1,10 @@
+
+
+export const AdminLayout = () => {
+  return (
+    <div className="flex min-h-screen w-full">
+        {/*ADMIN Sidebar  / Lado administrador*/}
+        <div className="flex flex-1 flex-col"></div>
+    </div>
+  )
+}
