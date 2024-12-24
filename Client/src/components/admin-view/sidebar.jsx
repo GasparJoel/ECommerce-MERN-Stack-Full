560f540f801ebf158cddd7d25dc08e809311ae80
Joel Gt <0332201030@unjfsc.edu.pe>
2024-12-24 00:35:38 -0500

    Integracion de sheet dialog para  crear un nuevo producto

--- Client/src/components/admin-view/sidebar.jsx
+++ Client/src/components/admin-view/sidebar.jsx
@@ -25,13 +25,19 @@ export const  adminSidebarMenuItems =[
   }
 ]
 
-  const MenuItems = ()=>{
+  const MenuItems = ({setOpen})=>{
     const navigate = useNavigate()
 
     return <nav className="mt-8 flex-col flex gap-2">
       {
         adminSidebarMenuItems.map((menuitem) =>
-        <div key={menuitem.id}  onClick={()=>navigate(menuitem.path)}
+        <div key={menuitem.id}  
+        onClick={()=>
+        {navigate(menuitem.path)
+        setOpen? setOpen(false):null
+        
+        }
+        }
          className="flex cursor-pointer items-center gap-2  rounded-md px-3 py-2 bg-white hover:text-primary-foreground hover:bg-primary/90">
             {menuitem.icon}
             <span> {menuitem.label}</span>
@@ -48,22 +54,25 @@ export const AdminSidebar = ({open,setOpen}) => {
   return (
     
     <Fragment>
-
+      {/* Este es cuando la pantalla es modo movil  */}
       <Sheet open={open} onOpenChange={setOpen}>
         <SheetContent  side="left" className="w-64">
           <div className="flex flex-col h-full">
             <SheetHeader className='border-b'>
-            <ChartNoAxesCombined size={30}/>
-              <SheetTitle>Admin Panel</SheetTitle>
+            
+              <SheetTitle className="flex gap-2 mt-5 mb-5">
+                  <ChartNoAxesCombined size={30}/>
+                  <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+                </SheetTitle>
             </SheetHeader>
-            <MenuItems/>
+            <MenuItems setOpen={setOpen}/>
 
           </div>
 
         </SheetContent>
 
       </Sheet>
-
+ {/* Este es cuando la pantalla es modo escritorio  */}
       <aside className=" hidden w-64 flex-col border-r bg-white p-6 lg:flex">
         <div onClick={()=>navigate('/admin/dashboard')} className="flex items-center gap-2 cursor-pointer ">
         <ChartNoAxesCombined size={30}/>
--- Client/src/pages/admin-view/products.jsx
+++ Client/src/pages/admin-view/products.jsx
@@ -1,6 +1,28 @@
+import { Button } from "@/components/ui/button"
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet"
+
+import { Fragment, useState } from "react"
 
 export const AdminProducts = () => {
-    return (
-      <div> Admin Products</div>
-    )
+
+    const [openCreateProductsDialog, setopenCreateProductsDialog] = useState(false)
+
+    return <Fragment>
+      <div className="mb-5 w-full  flex justify-end">
+        <Button onClick={()=>setopenCreateProductsDialog(true)} > Add New Product</Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
+        <Sheet open={openCreateProductsDialog} 
+        onOpenChange={()=>{
+          setopenCreateProductsDialog(false)
+        }}
+        >
+          <SheetContent side="right" className="overflow-auto">
+            <SheetHeader>
+              <SheetTitle>Add new Product</SheetTitle>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </Fragment>
   }
