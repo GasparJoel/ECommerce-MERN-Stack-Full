b97ddcd58617c9e1b6c2359a780e51043d846951
Joel Gt <0332201030@unjfsc.edu.pe>
2025-01-08 17:50:11 -0500

    Preparamos la funcion onchange del input type file para poder capturar los valores de las imagenes

--- Client/src/components/admin-view/image-upload.jsx
+++ Client/src/components/admin-view/image-upload.jsx
@@ -1,16 +1,31 @@
 
+import { useRef } from "react"
 import { Input } from "../ui/input"
 import { Label } from "../ui/label"
 
 
 export const ProductImageUpload = ({file , setImageFile, uploadedImageUrl, setuploadedImageUrl}) => {
+
+    const inputRef =useRef(null);
+
+    const handleImageFileChange =(e)=>{
+        console.log(e.target.files)
+    }
   return (
+
+
+
 
     <div className="w-full max-w-md mx-auto">
         <Label className="text-lg font-semibold mb-2 block">Upload Image</Label> 
 
         <div>
-            <Input  id="image-upload" type="file" className="hidden" />
+            <Input  
+            id="image-upload" 
+            type="file"
+            //  className="hidden" 
+             ref={inputRef} 
+             onChange={handleImageFileChange} />
         </div>
     </div>
   )
